243e6957b627ec6fd5e87ae13e3fe974b7f45ab4
Sashom <Alexander Mardirossian>
2017-02-11 11:29:11 +0200

    Initial Commit

---
+++ WMNetwork/WMNetwork.h
@@ -0,0 +1,19 @@
+//
+//  WMNetwork.h
+//  WMNetwork
+//
+//  Created by Sasho on 11.02.17 г..
+//  Copyright © 2017 г. WebMaster Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMNetwork.
+FOUNDATION_EXPORT double WMNetworkVersionNumber;
+
+//! Project version string for WMNetwork.
+FOUNDATION_EXPORT const unsigned char WMNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMNetwork/PublicHeader.h>
+
+
---
+++ WMNetworkTests/WMNetworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  WMNetworkTests.swift
+//  WMNetworkTests
+//
+//  Created by Sasho on 11.02.17 г..
+//  Copyright © 2017 г. WebMaster Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import WMNetwork
+
+class WMNetworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
